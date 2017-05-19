6949845db5c91d240d8e3bd776d129d33ec0c68a
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-19 10:49:01 -0500

    cambio de formato de mensaje

--- tablaResultado/tablaResultado.cpp
+++ tablaResultado/tablaResultado.cpp
@@ -8,13 +8,16 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE , LPTSTR cmdLine, int cmdSh
 
 void tablaResultado::Window_Open(Win::Event& e)
 {
-	int temperatura = 24;
+	int temperatura = 22;
 	int edad = 19;
-	int peso = 60;
+	int peso = 57;
 	wstring texto;
-	edad=10;
-	Sys::Format(texto, L"(%i,%d)->(%i,%i)", temperatura/2,5+temperatura,edad,peso-1);
+	edad=20;
+	peso /= 2;
+	temperatura = 18 * 2;
+	Sys::Format(texto, L"(%i,%i)->(%i,%i)", temperatura/3,5+temperatura,edad,peso+1);
 	this->MessageBox(texto, L"Resultado", MB_OK);
-	this->MessageBox(texto,L"Resultado",MB_OK);
+	this->MessageBox(L"Hola",texto,MB_OK);
+	this->MessageBox(texto,texto,MB_OK);
 }
 
